1d21da834e249f0915e03ae315be37efbcfa0974
Kazunori Sakamoto <exkazuu@gmail.com>
2012-04-04 14:46:27 +0900

    Update AssemblyInfo files.

--- Code2Xml.Core/Properties/AssemblyInfo.cs
+++ Code2Xml.Core/Properties/AssemblyInfo.cs
@@ -56,5 +56,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.3.4.80")]
-[assembly: AssemblyFileVersion("1.3.4.80")]
+[assembly: AssemblyVersion("1.3.4.81")]
+[assembly: AssemblyFileVersion("1.3.4.81")]
--- Code2Xml.Languages.CSharp/Properties/AssemblyInfo.cs
+++ Code2Xml.Languages.CSharp/Properties/AssemblyInfo.cs
@@ -57,4 +57,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("1.1.0.32")]
-[assembly: AssemblyFileVersion("1.1.0.32")]
+[assembly: AssemblyFileVersion("1.1.0.33")]
