7cf77fd5b8a35229fac5c9939dd95a66f06a2857
Kazunori Sakamoto <exkazuu@gmail.com>
2012-10-17 03:28:30 +0900

    Add packages for travis-ci.

--- Code2Xml.Languages.JavaScript/Properties/AssemblyInfo.cs
+++ Code2Xml.Languages.JavaScript/Properties/AssemblyInfo.cs
@@ -56,5 +56,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.1.0.46")]
-[assembly: AssemblyFileVersion("1.1.0.46")]
+[assembly: AssemblyVersion("1.1.0.48")]
+[assembly: AssemblyFileVersion("1.1.0.48")]
--- README.txt
+++
@@ -1,7 +0,0 @@
-# Requirements
-NuGet
-
-# How to build
-1. Right click the solution icon in the Solution Explorer.
-2. Click 'Enable NuGet Package Restore'.
-3. Build the solution.
