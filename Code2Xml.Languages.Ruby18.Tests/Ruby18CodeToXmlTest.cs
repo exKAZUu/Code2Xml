8a8185327901006ad0bf959c1aed103a2b6b804e
Kazunori Sakamoto <exkazuu@gmail.com>
2012-10-17 18:21:26 +0900

    Fix ruby test.

--- Code2Xml.Languages.Ruby18.Tests/Ruby18CodeToXmlTest.cs
+++ Code2Xml.Languages.Ruby18.Tests/Ruby18CodeToXmlTest.cs
@@ -27,7 +27,7 @@ namespace Code2Xml.Languages.Ruby18.Tests {
 	public class Ruby18CodeToXmlTest {
 		[Test]
 		public void コードを解析できる() {
-			Ruby18CodeToXml.Instance.Generate("a = 1");
+			Ruby18CodeToXml.Instance.Generate("p 1");
 			//var path = Fixture.GetInputPath(
 			//        "Ruby18", "block.rb");
 			//Ruby18CodeToXml.Instance.GenerateFromFile(path, true);
