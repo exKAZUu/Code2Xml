61a6edeb3c01b8ea8365eb00fb0fbd5a43dde863
Kazunori Sakamoto <exkazuu@gmail.com>
2012-12-07 21:37:10 +0900

    Update scripts for Travis-CI.

--- nuget.bat
+++ nuget.bat
@@ -1,8 +1,2 @@
-set MyDir=%~p0
 ".nuget/NuGet.exe" install NUnit.Runners -Version 2.6.1 -o packages
-
-for /D %%d in (*) do (
-    if exist %%d\packages.config (
-        "%MyDir%.nuget\NuGet.exe" install "%%d\packages.config" -o "%MyDir%packages"
-    )
-)
+".nuget/NuGet.exe" install Code2Xml.Languages.Tests\packages.config -o packages
--- nuget.sh
+++ nuget.sh
@@ -1,7 +1,3 @@
 #/bin/sh
 mono --runtime=v4.0.30319 .nuget/NuGet.exe install NUnit.Runners -Version 2.6.1 -o packages
-
-find . -type f -name packages.config | while read FILE
-do
-	mono --runtime=v4.0.30319 .nuget/NuGet.exe install ${FILE} -o packages
-done
+mono --runtime=v4.0.30319 .nuget/NuGet.exe install Code2Xml.Languages.Tests/packages.config -o packages
