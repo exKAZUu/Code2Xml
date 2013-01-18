3f13c4506d9590a4336f51605a60308a723f1e36
Kazunori Sakamoto <exkazuu@gmail.com>
2013-01-18 18:11:41 +0900

    Bump up version.

--- Code2Xml.Core/Properties/AssemblyInfo.cs
+++ Code2Xml.Core/Properties/AssemblyInfo.cs
@@ -56,5 +56,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.5.0.31")]
-[assembly: AssemblyFileVersion("1.5.0.31")]
+[assembly: AssemblyVersion("1.5.1.32")]
+[assembly: AssemblyFileVersion("1.5.1.32")]
