44fb8fa919f1fe1621dc05a0686d20b55a8ff59c
Kazunori Sakamoto <exkazuu@gmail.com>
2013-01-25 18:58:46 +0900

    Fix settings of Code Contracts.

--- Code2Xml.Core/Properties/AssemblyInfo.cs
+++ Code2Xml.Core/Properties/AssemblyInfo.cs
@@ -56,5 +56,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.6.0.44")]
-[assembly: AssemblyFileVersion("1.6.0.44")]
+[assembly: AssemblyVersion("1.6.1.45")]
+[assembly: AssemblyFileVersion("1.6.1.45")]
