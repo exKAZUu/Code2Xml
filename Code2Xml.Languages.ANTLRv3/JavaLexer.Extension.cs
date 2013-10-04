88b4c10a7af4d9883b6ff687940c057e143ff5c2
Kazunori Sakamoto <exkazuu@gmail.com>
2013-10-04 12:56:27 +0900

    Add Erlang parser.

---
+++ Code2Xml.Languages.ANTLRv3/JavaLexer.Extension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Antlr.Runtime;
+
+namespace Code2Xml.Languages.ANTLRv3
+{
+    public partial class JavaLexer
+    {
+		public Antlr3Listener Listener {
+			get; set;
+		}
+
+	    private int depth = 0;
+
+	    partial void EnterRule(string ruleName, int ruleIndex) {
+		    depth++;
+		    Console.WriteLine("Enter: " + ruleName);
+	    }
+
+	    partial void LeaveRule(string ruleName, int ruleIndex) {
+		    if (--depth == 0) {
+			    Listener.VisitTerminalNode(Text, ruleIndex, state.channel);
+		    }
+		    Console.WriteLine("Levave: " + ruleName);
+	    }
+    }
+}
---
+++ Code2Xml.Languages.ANTLRv3/JavaParser.Extension.cs
@@ -0,0 +1,45 @@
+﻿#region License
+
+// Copyright (C) 2011-2013 Kazunori Sakamoto
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using Antlr.Runtime;
+using Antlr.Runtime.Tree;
+
+namespace Code2Xml.Languages.ANTLRv3 {
+	public partial class JavaParser {
+		public Antlr3Listener Listener {
+			get; set;
+		}
+
+		partial void EnterRule(string ruleName, int ruleIndex) {
+			Listener.EnterNonTerminalNode(ruleName);
+			Console.WriteLine("Enter: " + ruleName);
+		}
+
+		partial void LeaveRule(string ruleName, int ruleIndex) {
+			Listener.LeaveNonTerminalNode(ruleName);
+			Console.WriteLine("Leave: " + ruleName);
+		}
+
+		partial void CreateTreeAdaptor(ref ITreeAdaptor adaptor) {
+		}
+	}
+}
