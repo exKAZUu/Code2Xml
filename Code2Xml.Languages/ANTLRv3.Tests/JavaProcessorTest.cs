44ad843be60e87380f9b5087760f89a75dd629a7
Kazunori SAKAMOTO <exkazuu@gmail.com>
2013-11-12 19:38:40 +0900

    Add tests which parse Java 7 code.

--- Code2Xml.Languages/ANTLRv3.Tests/JavaProcessorTest.cs
+++ Code2Xml.Languages/ANTLRv3.Tests/JavaProcessorTest.cs
@@ -22,14 +22,14 @@ using Code2Xml.Languages.ANTLRv3.Processors.Java;
 using NUnit.Framework;
 
 namespace Code2Xml.Languages.ANTLRv3.Tests {
-	[TestFixture]
-	public class JavaProcessorTest : ProcessorTest {
-		protected override Processor CreateProcessor() {
-			return new JavaProcessorUsingAntlr3();
-		}
+    [TestFixture]
+    public class JavaProcessorTest : ProcessorTest {
+        protected override Processor CreateProcessor() {
+            return new JavaProcessorUsingAntlr3();
+        }
 
-		[Test]
-		[TestCase(@"//test
+        [Test]
+        [TestCase(@"//test
 import javax.swing.*;
  
 public class Hello extends JFrame {
@@ -42,37 +42,43 @@ public class Hello extends JFrame {
         new Hello().setVisible(true);
     }
 }")]
-		[TestCase(@"class Main {
+        [TestCase(@"class Main {
   void test() { obj.method().<Object>method2(); }
 }")]
-		[TestCase(@"public class AlignedTuplePrinter {
+        [TestCase(@"public class AlignedTuplePrinter {
     List<String> columnLines = new ArrayList<>();
 }")]
-		[TestCase(@"class Klass { void main() {
+        [TestCase(@"class Klass { void main() {
 	try { } finally { }
 }}")]
-		[TestCase(@"class Klass { void main() {
+        [TestCase(@"class Klass { void main() {
 	try (Object obj = new Object()) { } finally { }
 }}")]
-		[TestCase(@"class Klass { void main() {
+        [TestCase(@"class Klass { void main() {
 	try (Object obj = new Object() ; Object obj = new Object()) { } finally { }
 }}")]
-		public void Parse(string code) {
-			VerifyRestoringCode(code);
-		}
+        public void Parse(string code) {
+            VerifyRestoringCode(code);
+        }
 
-		[Test]
-		public void ParseBrokenCodeIgnoringException() {
-			var code = @"class A {{ }";
-			var processor = new JavaProcessorUsingAntlr3();
-			processor.GenerateXml(code, false);
-		}
+        [Test]
+        [TestCase(@"presto")]
+        public void ParseDirectory(string directoryName) {
+            VerifyRestoringProjectDirectory("Java", directoryName, "*.java");
+        }
 
-		[Test, ExpectedException(typeof(ParseException))]
-		public void ParseBrokenCode() {
-			var code = @"class A {{ }";
-			var processor = new JavaProcessorUsingAntlr3();
-			processor.GenerateXml(code, true);
-		}
-	}
+        [Test]
+        public void ParseBrokenCodeIgnoringException() {
+            var code = @"class A {{ }";
+            var processor = new JavaProcessorUsingAntlr3();
+            processor.GenerateXml(code, false);
+        }
+
+        [Test, ExpectedException(typeof(ParseException))]
+        public void ParseBrokenCode() {
+            var code = @"class A {{ }";
+            var processor = new JavaProcessorUsingAntlr3();
+            processor.GenerateXml(code, true);
+        }
+    }
 }
