a359197a37b91ce914226987a89d2b3d4d81ba60
Sakamoto, Kazunori <exkazuu@gmail.com>
2015-09-27 11:33:17 +0900

    Update AstSerializer to 1.0.4.

--- Code2Xml.Core/Properties/AssemblyInfo.cs
+++ Code2Xml.Core/Properties/AssemblyInfo.cs
@@ -53,5 +53,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.3.4.100")]
-[assembly: AssemblyFileVersion("3.3.4.100")]
+[assembly: AssemblyVersion("3.3.5.101")]
+[assembly: AssemblyFileVersion("3.3.5.101")]
